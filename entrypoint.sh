e9a068baccfbaf361182b231993b51dd47451035
Oneal Anguin <oneal.anguin@ef.com>
2020-03-18 09:47:02 +0000

    * setup passing parameters to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -u
 
-echo "::set-output name=generated-doc-url::http://generated-doc"
-
 for f in schemas/*
 do
     file="$(basename -- $f)"
     redoc-cli bundle "schemas/${file}" --output "docs/${file%.*}.html" 
 done
 
-
+echo "::set-output name=generated-doc-url::http://generated-doc"
 echo "Finished creatings docs"
 exit 0
